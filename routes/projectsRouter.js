978a5ca1a9cbe8802691420ad6033cf19cda8cac
Brandon <Blent1050@yahoo.com>
2019-02-15 12:10:55 -0500

    Added put on projects

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
     const project = await Projects.get(req.params.id);
     res.status(200).json(project);
   } catch (error) {
-    res.status(500).json({ message: 'Error retrieving actions', error: error });
+    res.status(500).json({ message: 'Error retrieving Project', error: error });
   }
 });
 
@@ -29,6 +29,19 @@ router.post('/', async (req, res) => {
     res.status(200).json(project);
   } catch (error) {
     res.status(500).json({ message: 'Error posting Project', error: error });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updateProject = await Projects.update(req.params.id, req.body);
+    if (updateProject) {
+      res.status(200).json({ message: 'Updated the post!', newPost: req.body });
+    } else {
+      res.status(404).json({ Message: 'The user could not be found!' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error updated Project', error: error });
   }
 });
 
